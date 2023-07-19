e4bf2e55c2158ef389bb5191246bf7dbd3bce880
luck2jay <awkaejay97@mju.ac.kr>
2023-07-19 17:29:19 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/test/java/hello/core/singleton/StatefulService.java
    #	src/test/java/hello/core/singleton/StatefulServiceTest.java

--- src/main/java/hello/core/CoreApplication.java
+++ src/main/java/hello/core/CoreApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
     public class CoreApplication {
 
         public void main(String[] args) {
+
             SpringApplication.run(CoreApplication.class, args);
         }
 
